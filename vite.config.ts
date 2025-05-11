97dd25ce4f37a0a7a12ccac64fe2f777ac1b68ab
Ibrahima LO <bramslo17@gmail.com>
2025-05-11 22:18:11 +0000

    ONLINE DEP 1

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/br17-website/',
+  base: '/Bramslo/',
   plugins: [  
     vue(),
     vueDevTools(),
